62d7a35f3ba66dee63d0abf7f780971b8ff1dc19
richardMembreno <68753173+richardMembreno@users.noreply.github.com>
2022-04-10 23:33:40 -0600

    Create viewApi.php

---
+++ views/viewApi.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+    <div class="card text-dark mt-4 ms-3" style="max-width: 18rem;background-color:#4ADEDE;">
+        <div class="card-body">
+            <h5 class="card-title" id="cardText"></h5>
+        </div>
+    </div>
+</div>
